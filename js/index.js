4fba80c11e8ee31cc8fe2e397c80d4d6bce57799
unknown <ar7hurbr@gmail.com>
2020-06-21 17:23:30 -0300

    enxugando o código do JS

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ var pontosEl = document.getElementById("pontos");
 var numeroInicial = 0;
 var help = 0;
 
-function maisUm(){
+function maisNumero(numero){
     document.getElementById("numeroAnterior").innerHTML = numeroInicial;
-    numeroInicial++;
+    numeroInicial = numeroInicial + numero;
     document.getElementById("pontos").innerHTML = numeroInicial;
     if(numeroInicial==21){
         alert("Parabéns! você ganhou essa!");
@@ -24,34 +24,6 @@ function maquina(){
     }else if(numeroInicial > 21){
         alert("Você ganhou pq a máquina é mais burra do que você!");
         help == 1;
-    }
-}
-
-function maisDois(){
-    document.getElementById("numeroAnterior").innerHTML = numeroInicial;
-    numeroInicial = numeroInicial + 2;
-    document.getElementById("pontos").innerHTML = numeroInicial;
-    if(numeroInicial==21){
-        alert("Parabéns! você ganhou essa!");
-    }else if(numeroInicial <= 20){
-        maquina();
-    }
-    if((numeroInicial > 21) &&(help == 0)){
-        alert("Como você conseguiu perder pra máquina? Tu é muito burro!");
-    }
-}
-
-function maisTres(){
-    document.getElementById("numeroAnterior").innerHTML = numeroInicial;
-    numeroInicial = numeroInicial + 3;
-    document.getElementById("pontos").innerHTML = numeroInicial;
-    if(numeroInicial==21){
-        alert("Parabéns! você ganhou essa!");
-    }else if(numeroInicial <= 20){
-        maquina();
-    }
-    if((numeroInicial > 21) &&(help == 0)){
-        alert("Como você conseguiu perder pra máquina? Tu é muito burro!");
     }
 }
 
